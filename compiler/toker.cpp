5de11af55b9fca334d1dad67cae424b899540035
Chris A <cv565942@gmail.com>
2026-07-14 10:18:11 -0500

    add BX3D define

--- compiler/toker.cpp
+++ compiler/toker.cpp
@@ -124,6 +124,7 @@ Toker::Toker(const std::string& file, std::istream& in, bool debug, bool preproc
     if (preprocess) {
         MacroDefines["__DEBUG__"] = debug ? "True" : "False";
         MacroDefines["__VERSION__"] = BASE_VER;
+        MacroDefines["_B3XD"] = "1";
     }
     makeKeywords();
     nextline();
