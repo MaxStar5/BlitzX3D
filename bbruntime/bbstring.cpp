a5625e52a05cfbfb792009e03558be6c74748b82
MaxStar5 <101726008+MaxStar5@users.noreply.github.com>
2026-08-30 07:55:37 +0200

    Rename ChrAscii to ChrRaw

--- bbruntime/bbstring.cpp
+++ bbruntime/bbstring.cpp
@@ -120,7 +120,7 @@ BBStr* bbChr(int chr) {
 	return result;
 }
 
-BBStr* bbChrAscii(int n) {
+BBStr* bbChrRaw(int n) {
 	BBStr* t = new BBStr();
 	*t += (char)n;
 	return t;
@@ -202,7 +202,7 @@ void string_link(void(*rtSym)(const char*, void*)) {
 	rtSym("$LSet$string%size", bbLSet);
 	rtSym("$RSet$string%size", bbRSet);
 	rtSym("$Chr%unicode", bbChr);
-	rtSym("$ChrAscii%ascii", bbChrAscii);
+	rtSym("$ChrRaw%byte", bbChrRaw);
 	rtSym("%Asc$string", bbAsc);
 	rtSym("%Len$string", bbLen);
 	rtSym("$Hex%value", bbHex);
